9f2ed3688cb289cfe31e052f2c9b5828720cb6a1
abelgeorge323 <102869614+abelgeorge323@users.noreply.github.com>
2025-10-19 21:02:02 -0400

    Delete MIT_Dashboard_Streamlit directory

--- MIT_Dashboard_Streamlit/utils/helpers.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def metric_card(container, label, value, link=None):
-    if link:
-        if st.button(label, key=label):
-            st.switch_page(link)
-    else:
-        container.metric(label, value)
